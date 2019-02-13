3b4bb624c3c61eeeb5dcd4f243b0b2df632aa190
Alex Loftus <alexloftus2004@gmail.com>
2019-02-13 11:14:33 -0500

    made discrim file. In base directory for now, gonna move it to its permanent home when its done

---
+++ discriminability.py
@@ -0,0 +1,28 @@
+""" 
+Discriminability code. Takes in output of ndmg participant-level analysis.
+"""
+
+import os
+
+def get_graph_files():
+    """
+    Input: ndmg participant-level analysis output directory.
+    Returns: folder locations for all graph outputs
+    """
+    pass
+
+
+def numpy_from_graph():
+    """ 
+    Input: location of the .csv file for a single ndmg graph output
+    Returns: numpy array from that .csv file
+    """
+    pass
+    
+
+def matrix_and_vector_from_graph():
+    """ 
+    Input: List of graph output locations
+    Returns: the tuple (matrix, target_vector).
+    """
+    pass
