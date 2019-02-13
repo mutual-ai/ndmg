cab80422d965ce88d1737b782464184f8f62d53b
Alex Loftus <alexloftus2004@gmail.com>
2019-02-13 12:26:24 -0500

    finished get_graph_files function

--- discriminability.py
+++ discriminability.py
@@ -8,13 +8,13 @@ import os, sys, re
 def get_graph_files(ndmg_participant_dir):
     """
     Input: ndmg participant-level analysis output directory.
-    Returns: folder locations for all graph outputs
+    Returns: list of folder locations for all graph outputs
     """
-    d = os.path.abspath(ndmg_participant_dir)  # to make coding easier
-    for foldername, subfolders, filenames in os.walk(d):
-        for filename in filenames:
-            if filename.endswith('adj.csv'):
-                print(filename)
+    d = os.path.abspath(ndmg_participant_dir)  # to make things easier
+    out = [os.path.join(directory, filename)  # Returns list of absolute path files in ndmg_participant_dir that end in '_adj.csv'.
+          for directory, _, filenames in os.walk(d) 
+          for filename in filenames if filename.endswith('_adj.csv')]
+    return out
 
 data = 'scratch/02-12-NKI-ac0bc77-3'
 get_graph_files(data)
@@ -38,8 +38,8 @@ def matrix_and_vector_from_graph():
 
 #%%
 def main():
-    # data = 'scratch/02-12-NKI-ac0bc77-3'
-    # get_graph_files(data)
+    # TODO: input participant dir, output matrix_and_vector_from_graph() csv files
+    pass
 
 if __name__ == "__main__":
     main()
